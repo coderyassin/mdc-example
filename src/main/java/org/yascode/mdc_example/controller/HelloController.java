c97e5e3c6da2dcdd73347592f6da0d2ba371afad
Yassin MELLOUKI <yassin.mellouki.j@gmail.com>
2024-07-14 12:24:01 +0100

    Feat: test cors filter

--- src/main/java/org/yascode/mdc_example/controller/HelloController.java
+++ src/main/java/org/yascode/mdc_example/controller/HelloController.java
@@ -1,14 +1,23 @@
 package org.yascode.mdc_example.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.MDC;
 import org.slf4j.Marker;
 import org.slf4j.MarkerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -39,5 +48,23 @@ public class HelloController {
     public String greet(@RequestParam(value = "name", defaultValue = "World") String name) {
         //log.info("Handling /greet request for {}, traceId {}", name, MDC.get("traceId"));
         return "Greetings from Spring Cloud Sleuth!";
+    }
+
+    @GetMapping("/authenticate")
+    public Object authenticate() {
+        String url = "http://localhost:6689/api/v1/admin/resource";
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJhdXRob3JpdGllcyI6WyJSRUFEX1BSSVZJTEVHRSIsIlVQREFURV9QUklWSUxFR0UiLCJXUklURV9QUklWSUxFR0UiLCJST0xFX01BR0lDIiwiREVMRVRFX1BSSVZJTEVHRSJdLCJyb2xlcyI6WyJNQUdJQyJdLCJzdWIiOiJSRnZIckVoZSIsImlhdCI6MTcyMDk1NjAyOSwiZXhwIjoxNzIwOTU2MjY5LCJuYmYiOjE3MjA5NTYwMzB9.EyGQH8KOHfb6pF68oW09LMqAlWs5PuMxkVdcC1grsi0";
+        MultiValueMap<String, String> headers = new LinkedMultiValueMap<>(Map.of("Authorization",  List.of("Bearer " + token)));
+        HttpHeaders requestHeaders = new HttpHeaders(headers);
+        HttpEntity requestEntity = new HttpEntity<>(requestHeaders);
+        try {
+            ResponseEntity<String> responseEntity = restTemplate.exchange(url, HttpMethod.GET, requestEntity, String.class);
+            if (responseEntity.getStatusCode().is2xxSuccessful()) {
+                return responseEntity.getBody();
+            }
+        } catch (RestClientException e) {
+            log.error(e.getMessage());
+        }
+        return null;
     }
 }
